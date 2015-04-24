15b01765a38cd4bbc37436d7d737a584a4b19884
Neeraj Mittal <mittal.neeraj@gmail.com>
2015-04-24 18:34:38 +0530

    changed from ats to js file extension

--- karma-js.conf.js
+++ karma-js.conf.js
@@ -12,24 +12,22 @@ module.exports = function(config) {
             {pattern: 'test-main.js', included: true},
 
             // All the specs and sources are included dynamically from `test/main.js`.
-            {pattern: 'src/**/*.ats', included: false},
-            {pattern: 'test/**/*.ats', included: false},
+            {pattern: 'src/**/*.js', included: false},
+            {pattern: 'test/**/*.js', included: false},
 
             // The runtime assertion library.
             {pattern: 'node_modules/rtts-assert/dist/amd/assert.js', included: false}
         ],
 
         preprocessors: {
-            '**/*.ats': ['traceur']
+            'src/**/*.js': ['traceur'],
+            'test/**/*.js': ['traceur']
         },
 
         browsers: ['Chrome'],
 
         traceurPreprocessor: {
-            options: traceurOptions,
-            transformPath: function(path) {
-                return path.replace(/\.ats$/, '.js');
-            }
+            options: traceurOptions
         }
     });
 };
